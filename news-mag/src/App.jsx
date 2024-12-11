2948e94faebea3b52cde62595d7cba1eb4947905
ritik <iamritiknagar@gmail.com>
2024-12-11 21:15:31 +0530

    my project

---
+++ news-mag/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import Navbar from "./Components/Navbar"
+import NewsBord from "./Components/NewsBord"
+
+
+
+
+
+const App = () => {
+
+
+  const [category,setCategory] = useState("general");
+  return (
+    <div>
+    <Navbar setCategory={setCategory}/>
+    <NewsBord category={category}/>
+  </div>
+  )
+}
+
+export default App
---
+++ news-mag/src/Components/NewsBord.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import NewsItem from "./NewsItem";
+
+
+
+
+const NewsBord = ({category}) => {
+
+    const [articles,setArticles] = useState([]);
+
+    useEffect(()=> {
+        let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));    
+    },[category])
+  return (
+    <div style={{marginLeft:"50px"}}>
+        <h2 className="text-center">latest <span className="badge bg-danger">news</span></h2>
+        {articles.map((news,index)=>{
+            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+        })}
+    </div>  
+  )
+
+}
+
+
+export default NewsBord
